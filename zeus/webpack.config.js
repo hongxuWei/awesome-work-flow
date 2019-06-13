2f23085a216e9f5a42326633da45b50297b9c83c
Hongxu Wei <weihongxu002@ke.com>
2019-06-13 14:32:04 +0800

    add: webpack config

--- zeus/webpack.config.js
+++ zeus/webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -5,5 +6,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'bin'),
     filename: 'zeus.js'
-  }
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'node-loader'
+      }
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: "#!/usr/bin/env node\n",
+      raw: true,
+    })
+  ]
 };
