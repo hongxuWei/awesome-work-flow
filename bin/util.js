01f375218ebdb99d411248559d97cb50c11c800d
Hongxu Wei <weihongxu002@ke.com>
2019-08-29 16:06:02 +0800

    [WORK-FLOW] build 输出过长出错的问题,暂定 maxBuffer 为 1M

--- bin/util.js
+++ bin/util.js
@@ -3,13 +3,23 @@ const { exec } = require('child_process')
 
 // 返回一个 exec promise
 const execPromise = (action) => new Promise((resolve) => {
-  exec(action, (error) => {
+  const res = exec(action, {
+    maxBuffer: 1024 * 1024
+  },(error) => {
     if (error !== null) {
       console.log(chalk.red(error))
       resolve(false)
     }
     resolve(true)
   })
+
+  res.stdout.on('data', (data) => {
+    console.log(data);
+  });
+
+  res.stderr.on('data', (data) => {
+    console.log(data);
+  });
 })
 
 module.exports = {
